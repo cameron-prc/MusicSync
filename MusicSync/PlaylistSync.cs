b351d00d944c2e7f57808c8bc7c51bee31f1089f
cameron-prc <cameron.prc@gmail.com>
2021-10-26 10:04:34 +1300

    Add basic project skeleton

---
+++ MusicSync/PlaylistSync.cs
@@ -0,0 +1,11 @@
+using System.Threading.Tasks;
+
+namespace MusicSync;
+
+public class PlaylistSyncer
+{
+    public async Task Run(string[] args)
+    {
+        await Task.FromResult(true);
+    }
+}
---
+++ MusicSync/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MusicSync;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var host = CreateHostBuilder(args).Build();
+
+        using var scope = host.Services.CreateScope();
+        var appRoot = scope.ServiceProvider.GetRequiredService<PlaylistSyncer>();
+
+        await appRoot.Run(args);
+    }
+
+    static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((_, configuration) =>
+            {
+                configuration.Sources.Clear();
+
+                configuration
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddEnvironmentVariables();
+            })
+            .ConfigureLogging(logging => { logging.AddConsole(); })
+            .ConfigureServices((_, services) =>
+            {
+                services.AddTransient<PlaylistSyncer>();
+            });
+    }
+}
