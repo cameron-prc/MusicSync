813749892bc9432b5d51b60b31163f5a252ccde8
cameron-prc <cameron.prc@gmail.com>
2023-05-18 10:42:20 +1200

    Move SearchTracks to IRemotePlaylistService
    This is not specific to a remote service so should not be in the base
    interface.

--- MusicSync/RemoteServices/IRemotePlaylistService.cs
+++ MusicSync/RemoteServices/IRemotePlaylistService.cs
@@ -6,6 +6,7 @@ namespace MusicSync.RemoteServices;
 
 public interface IRemotePlaylistService : IRemoteService
 {
+    public Task<RemoteTrack?> SearchTracks(TrackEntity track);
     public Task<IEnumerable<RemoteTrack>> GetPlaylist(string playlistId);
     public Task AddToPlaylist(string playlistId, IList<TrackEntity> tracks);
 }
--- MusicSync/RemoteServices/IRemoteService.cs
+++ MusicSync/RemoteServices/IRemoteService.cs
@@ -1,13 +1,9 @@
-using System.Threading.Tasks;
-using MusicSync.Common;
-
 namespace MusicSync.RemoteServices;
 
 public interface IRemoteService
 {
-    public Task<RemoteTrack?> SearchTracks(TrackEntity track);
-
     public ServiceType Type();
+
     public enum ServiceType
     {
         YouTube,
