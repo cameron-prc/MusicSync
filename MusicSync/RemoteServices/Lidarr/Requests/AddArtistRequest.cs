24b7898aaccfebaa16806291043379250a2db3e8
cameron-prc <cameron.prc@gmail.com>
2023-08-07 11:59:34 +1200

    fu add artist request

--- MusicSync/RemoteServices/Lidarr/Requests/AddArtistRequest.cs
+++ MusicSync/RemoteServices/Lidarr/Requests/AddArtistRequest.cs
@@ -1,3 +1,30 @@
+using System.Text.Json.Serialization;
+
 namespace MusicSync.RemoteServices.Lidarr.Requests;
 
-public record AddArtistRequest();
+public record AddArtistRequest
+{
+    [JsonPropertyName("rootFolderPath")]
+    private string RootFolderPath { get; set; }
+
+    [JsonPropertyName("monitor")]
+    private string Monitor { get; set; }
+
+    [JsonPropertyName("foreignArtistId")]
+    private string ForeignArtistId { get; set; }
+
+    [JsonPropertyName("monitorNewItems")]
+    private string MonitorNewItems { get; set; }
+
+    [JsonPropertyName("qualityProfileId")]
+    private string QualityProfileId { get; set; }
+
+    [JsonPropertyName("metadataProfileId")]
+    private string MetadataProfileId { get; set; }
+
+    [JsonPropertyName("tags")]
+    private string Tags { get; set; }
+
+    [JsonPropertyName("searchForMissingAlbums")]
+    private string SearchForMissingAlbums { get; set; }
+}
